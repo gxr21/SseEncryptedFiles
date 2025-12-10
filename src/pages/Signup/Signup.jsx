31015b108dbde9a13ac4bac8b6636eb88b280817
ALI <alijalal200311@gmail.com>
2025-12-10 19:27:50 +0300

    recorrect height and width signup

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,32 +1,48 @@
-import './Signup.css';
+import "./Signup.css";
 function Signup() {
   return (
-    <div className="main">
-        .
-      <div className="sec">
-        <h1 className='h2'>ليس لديك حساب من قبل ؟</h1>
-        <p className='p'>قم بأنشاء حساب الان</p>
-        <button className='signin'>انشاء حساب</button>
-        <div className="child">
-         <div className="inside">
-          <h1 className='h1'>تسجيل الدخول</h1>
-          <br />
-          <p>البريد الالكتروني </p>
-          <br />
-          <input type="email" />
-          <br />
-          <p>كلمة المرور</p>
-          <br />
-          <input type="password" />
-          <br />
-          <button className='signup'>تسجيل الدخول</button>
-         
-            
-         </div>   
+    <>
+    {/* === Background Here ! === */}
+    {/* ====  بعدين اذا حبينة نفعلها هاي الخلفية لا تحذف الكود ال موجود ب مال خلفية  css =====*/}
+    
+        {/* <div className="encryption-bg">
+        <div className="encryption-particles">
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        </div>
 
+        <div className="binary-float">0101010011010101010</div>
+        <div className="binary-float">1010010110100101100</div>
+        <div className="binary-float">0011010101101010101</div>
+        <div className="scan-line"></div>
+        <div className="corner-glow top-left"></div>
+        <div className="corner-glow bottom-right"></div>
+        </div> */}
+
+      <div className="signup-container">
+      <div className="signup-card">
+        <div className="signup-side">
+          <h1 className="signup-side-title">ليس لديك حساب ؟</h1>
+          <p className="signup-side-subtitle">قم بإنشاء حساب الآن</p>
+          <button className="signup-side-button">إنشاء حساب</button>
+        </div>
+        <div className="signup-form-wrapper">
+          <div className="signup-form">
+            <h1 className="signup-title">تسجيل الدخول</h1>
+            <label className="signup-label">البريد الإلكتروني</label>
+            <input type="email" className="signup-input" dir="rtl"/>
+            <label className="signup-label">كلمة المرور</label>
+            <input type="password" className="signup-input" dir="rtl" />
+            <button className="signup-button">تسجيل الدخول</button>
+          </div>
         </div>
       </div>
     </div>
+    </>
+    
   );
 }
 
