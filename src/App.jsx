90420616ef3eb208bca69489f0ec1dac71253252
ALI <alijalal200311@gmail.com>
2025-12-13 08:36:40 +0300

    add protactedRoute for more secure to dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,9 @@
 // ==================================
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import WelcomePage from "./pages/welcom_page/welcompage.jsx";
-import Signin from "./pages/Signin/SIgnin.jsx";
+import Signin from "./pages/Signin/Signin.jsx";
 import Signup from "./pages/Signup/Signup.jsx";
+import ProtectedRoute from "./utils/ProtectedPage/protected.jsx";
 import Dashboard from "./pages/Dashboard/dashboard.jsx";
 import "./index.css";
 function App() {
@@ -15,7 +16,10 @@ function App() {
         <Route path="/signin" element={<Signin/>} />
         <Route path="/signup" element={<Signup/>} />
         {/* === Protected Routes === */}
-
+        {/* <Route path='/dashboard' element={
+        <ProtectedRoute>
+          <Dashboard/>
+        </ProtectedRoute>}/> */}
         <Route path='/dashboard' element={<Dashboard/>} /> 
       </Routes>
     </BrowserRouter>
