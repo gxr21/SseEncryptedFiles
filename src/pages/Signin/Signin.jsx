e3ef26a94c92166fc4736dae528c1ca2803d9e80
moslem <yan451961@gmail.com>
2025-12-14 22:24:49 +0300

    changes

--- src/pages/Signin/Signin.jsx
+++ src/pages/Signin/Signin.jsx
@@ -1,54 +1,64 @@
-
+import axios from "axios";
+import { useState } from "react";
 import "./Signin.css";
-function Signup() {
+function Signin() {
+   const [email,setEmail] = useState("")
+   const [password,setPassword]=useState("")
+   
+  const HandleSubmit = async (e) => {
+  e.preventDefault();
+  const data = {email,password};
+
+  try {
+    const response = await axios.post(
+      "http://localhost:5000/signin",data);
+
+    console.log("الرد من السيرفر:", response.data);
+
+  } catch (error) {
+    console.error("خطأ:", error.response?.data || error.message);
+  }
+};
+  
   return (
     <>
+    <form onSubmit={HandleSubmit}>
     
-    {/* === Background Here ! === */}
-    {/* ====  بعدين اذا حبينة نفعلها هاي الخلفية لا تحذف الكود ال موجود ب مال خلفية  css =====*/}
-    
-        {/* <div className="encryption-bg">
-        <div className="encryption-particles">
-        <div className="particle"></div>
-        <div className="particle"></div>
-        <div className="particle"></div>
-        <div className="particle"></div>
-        <div className="particle"></div>
-        </div>
-
-        <div className="binary-float">0101010011010101010</div>
-        <div className="binary-float">1010010110100101100</div>
-        <div className="binary-float">0011010101101010101</div>
-        <div className="scan-line"></div>
-        <div className="corner-glow top-left"></div>
-        <div className="corner-glow bottom-right"></div>
-        </div> */}
-
-      <div className="signup-container">
-      <div className="signup-card">
+      <div className="signin-container">
+      <div className="signin-card">
         <div className="signup-side">
           <h1 className="signup-side-title">ليس لديك حساب ؟</h1>
           <p className="signup-side-subtitle">قم بإنشاء حساب الآن</p>
-          <a href="/signup">
+          <a href="/signin">
           <button className="signup-side-button">إنشاء حساب</button>
           </a>
-          
         </div>
-        <div className="signup-form-wrapper">
-          <div className="signup-form">
-            <h1 className="signup-title">تسجيل الدخول</h1>
-            <label className="signup-label">البريد الإلكتروني</label>
-            <input type="email" className="signup-input" dir="rtl"/>
-            <label className="signup-label">كلمة المرور</label>
-            <input type="password" className="signup-input" dir="rtl" />
-            <button className="signup-button">تسجيل الدخول</button>
+        <div className="signin-form-wrapper">
+          <div className="signin-form">
+            <h1 className="signin-title">تسجيل الدخول</h1>
+            <label className="signin-label">البريد الإلكتروني</label>
+            <input type="email" 
+            className="signin-input" 
+            dir="rtl"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            />
+            <label className="signin-label">كلمة المرور</label>
+            <input type="password" 
+            className="signin-input" 
+            dir="rtl" 
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            />
+            <button className="signin-button" type="submit">تسجيل الدخول</button>
           </div>
         </div>
       </div>
     </div>
+    </form>
     </>
     
   );
 }
 
-export default Signup;
+export default Signin;
--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,59 +1,127 @@
+import axios from "axios";
 import "./Signup.css";
-function Signin() {
+import { useState } from "react";
+
+function Signup() {
+
+  const [fullName, setFullName] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+     // منع اسم المستخدم بالعربي
+    if (/[\u0600-\u06FF]/.test(username)) {
+      setError("اسم المستخدم يجب أن يكون باللغة الإنجليزية فقط");
+      return;
+    }
+    //تأكيد كلمة المرور
+    if (password !== confirmPassword) {
+      setError("كلمتا المرور غير متطابقتين");
+      return;
+    }
+    const data = {  fullName,username,email,password};
+   try {
+    //ارسال البيانات بعد التحقق
+    const response = await axios.post(
+      "http://localhost:5000/signup",data);
+
+    console.log("الرد من السيرفر:", response.data);
+
+  } catch (error) {
+    console.error("خطأ:", error.response?.data || error.message);
+  }
+    
+    console.log("البيانات المرسلة:", data);
+    
+  };
+
   return (
     <>
-    {/* === Background Here ! === */}
-    {/* ====  بعدين اذا حبينة نفعلها هاي الخلفية لا تحذف الكود ال موجود ب مال خلفية  css =====*/}
-    
-        {/* <div className="encryption-bg">
-        <div className="encryption-particles">
-        <div className="particle"></div>
-        <div className="particle"></div>
-        <div className="particle"></div>
-        <div className="particle"></div>
-        <div className="particle"></div>
-        </div>
+      <form onSubmit={handleSubmit}>
 
-        <div className="binary-float">0101010011010101010</div>
-        <div className="binary-float">1010010110100101100</div>
-        <div className="binary-float">0011010101101010101</div>
-        <div className="scan-line"></div>
-        <div className="corner-glow top-left"></div>
-        <div className="corner-glow bottom-right"></div>
-        </div> */}
-        
-  {/* ====== هنا اريد الصفحة form يعني حتى المعلمات تنتقل للباك وتتعالج ======== */}
-      <div className="signin-container">
-      <div className="signin-card">
-        <div className="signin-side">
-          <h1 className="signup-side-title">لديك حساب من قبل ؟</h1>
-          <p className="signup-side-subtitle">قم بتسجيل الدخول</p>
-          <a href="/signin">
-          <button className="signup-side-button"> تسجيل الدخول</button>
-          </a>
-        </div>
-        <div className="signin-form-wrapper">
-          <div className="signin-form">
-            <h1 className="signin-title">انشاء حساب</h1>
-            {/* ==== اريد رسالة تظهر فوك الادخال انو اذا المستخدم كتب اسمة بالعربي ممنوع الاسم فقط بالانكليزي ==== */}
-            <label className="signin-label1">الأسم بالكامل</label>
-            <input type="text" className="signin-input1" dir="rtl"/>
-            <label className="signin-label1"> أسم المستخدم</label>
-            <input type="text" className="signin-input1" dir="rtl"/>
-            <label className="signin-label1">البريد الإلكتروني</label>
-            <input type="email" className="signin-input2" dir="rtl"/>  
-            <label className="signin-label2">كلمة المرور</label>
-            <input type="password" className="signin-input3" dir="rtl" />
-            <label className="signin-label3">تأكيد كلمة المرور</label>
-             <input type="password" className="signin-input4" dir="rtl" />
-            <button className="signin-button">انشاء حساب </button>
+        <div className="signup-container">
+          <div className="signup-card">
+
+            <div className="signin-side">
+              <h1 className="signin-side-title">لديك حساب من قبل ؟</h1>
+              <p className="signin-side-subtitle">قم بتسجيل الدخول</p>
+              <a href="/signin">
+                <button type="button" className="signin-side-button">
+                  تسجيل الدخول
+                </button>
+              </a>
+            </div>
+
+            <div className="signup-form-wrapper">
+              <div className="signup-form">
+
+                <h1 className="signup-title">انشاء حساب</h1>
+
+                {error && <p style={{ color: "red" }}>{error}</p>}
+
+                <label className="signup-label1">الأسم بالكامل</label>
+                <input
+                  type="text"
+                  className="signup-input1"
+                  dir="rtl"
+                  value={fullName}
+                  onChange={(e) => setFullName(e.target.value)}
+                />
+
+                <label className="signup-label1"> أسم المستخدم</label>
+                <input
+                  type="text"
+                  className="signup-input1"
+                  dir="rtl"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+
+                <label className="signup-label1">البريد الإلكتروني</label>
+                <input
+                  type="email"
+                  className="signup-input2"
+                  dir="rtl"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+
+                <label className="signup-label2">كلمة المرور</label>
+                <input
+                  type="password"
+                  className="signup-input3"
+                  dir="rtl"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+
+                <label className="signup-label3">تأكيد كلمة المرور</label>
+                <input
+                  type="password"
+                  className="signup-input4"
+                  dir="rtl"
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                />
+
+                <button className="signup-button" type="submit">
+                  انشاء حساب
+                </button>
+
+              </div>
+            </div>
+
           </div>
         </div>
-      </div>
-    </div>
+
+      </form>
     </>
-    
   );
 }
 
-export default Signin;
+export default Signup;
